149b1134ddd819adb1f090a5b66f2b571b628450
Asadbek <106092435+AsadbekMobileDeveloper@users.noreply.github.com>
2023-04-12 23:19:29 +0500

    deleted

--- lib/utils/my_utils.dart
+++
@@ -1,9 +0,0 @@
-import 'package:spam_request/export_files.dart';
-
-class MyUtils {
-  static void fieldFocusChange(
-      BuildContext context, FocusNode currentFocus, FocusNode nextFocus) {
-    currentFocus.unfocus();
-    FocusScope.of(context).requestFocus(nextFocus);
-  }
-}
